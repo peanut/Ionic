c2b02835365b2d752698bcd672f886074040b448
peanut <295062608@qq.com>
2018-04-03 21:27:18 +0800

    1

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { LoginPage } from "../login/login";
+
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tabRoots: Object[];
+
+  constructor() {
+    this.tabRoots = [
+      {
+        root: HomePage,
+        tabTitle: 'Home',
+        tabIcon: 'home'
+      },
+      {
+        root: ContactPage,
+        tabTitle: 'Contact',
+        tabIcon: 'notifications'
+      },
+      {
+        root: AboutPage,
+        tabTitle: 'About',
+        tabIcon: 'document'
+      },
+      {
+        root: LoginPage,
+        tabTitle: "Login",
+        tabIcon: "cart"
+      }
+    ];
+  }
+}
